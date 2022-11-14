aeb30c58f25bf57d45bf6752f0b54fd51b33b338
Jeff Zohrab <jzohrab@gmail.com>
2022-11-13 20:28:02 -0600

    Initial (sample) smoke test of session get_tags().

--- tests/session_utility_Test.php
+++ tests/session_utility_Test.php
@@ -6,10 +6,33 @@ use PHPUnit\Framework\TestCase;
 
 final class session_utility_Test extends TestCase
 {
-    
-    public function test_dummy_test(): void
+
+    /**
+     * Hack/fake session and server information.
+     */
+    public function setUp(): void
     {
-        $this->assertSame(1, 1);
+        $_SERVER['HTTP_HOST'] = 'localhost';
+        $_SERVER['REQUEST_URI'] = '/dummyuri';
+    }
+
+    public function tearDown(): void
+    {
+        // echo "tearing down ... \n";
+    }
+
+    public function test_smoke_tests_tags(): void
+    {
+        $session_keys = [ 'TAGS', 'TBPREF_TAGS' ];
+        foreach($session_keys as $k) {
+            unset($_SESSION[$k]);
+            $this->assertFalse(isset($_SESSION[$k]), 'sanity check');
+        };
+        $t = get_tags();
+        $this->assertIsArray($t, 'returns tags');
+        foreach($session_keys as $k) {
+            $this->assertTrue(isset($_SESSION[$k]), "Session {$k} set");
+        };
     }
 
 }
