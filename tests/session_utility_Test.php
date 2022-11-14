9144c6e07366d6cc3377461afc603f354dde81fd
Jeff Zohrab <jzohrab@gmail.com>
2022-11-13 20:00:26 -0600

    Stub file for testing session_utility.

---
+++ tests/session_utility_Test.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../inc/session_utility.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class session_utility_Test extends TestCase
+{
+    
+    public function test_dummy_test(): void
+    {
+        $this->assertSame(1, 1);
+    }
+
+}
