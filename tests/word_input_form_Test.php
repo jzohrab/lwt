e8b42bfddee4520c6a64f160ef7213327b63c560
Jeff Zohrab <jzohrab@gmail.com>
2022-11-21 21:03:50 -0600

    Clean up tests.

--- tests/word_input_form_Test.php
+++ tests/word_input_form_Test.php
@@ -15,22 +15,25 @@ final class word_input_form_Test extends TestCase
         DbHelpers::clean_db();
         do_mysqli_query("ALTER TABLE words AUTO_INCREMENT = 1");
 
+        $this->child = $this->make_formdata("CHILD");
+        $this->parent = $this->make_formdata("PARENT");
+        $this->stepdad = $this->make_formdata("STEPDAD");
+    }
+
+    private function make_formdata($term) {
         $f = new FormData();
         $f->lang = 1;
         // $f->wid = 0;
-        $f->term = 'HELLO';
-        $f->termlc = 'hello';
-        $f->translation = 'trans';
-        $f->romanization = 'rom';
-        $f->sentence = 'sent';
+        $f->term = $term;
+        $f->termlc = strtolower($term);
+        $f->translation = 'trans ' . $term;
+        $f->romanization = 'rom ' . $term;
+        $f->sentence = 'sent ' . $term;
         $f->status = 3;
         $f->status_old = 1;
-        // $f->parent_id = $parentid;
-        // $f->parent_text = 'HELLOPARENT';
-
-        $this->formdata = $f;
+        return $f;
     }
-
+    
     public function tearDown(): void
     {
         // echo "tearing down ... \n";
@@ -38,23 +41,18 @@ final class word_input_form_Test extends TestCase
 
     public function test_save_new_no_parent()
     {
-        save_new_formdata($this->formdata);
+        $this->child->parent_id = 0;
+        save_new_formdata($this->child);
         $sql = 'select WoID, WoText from words';
-        DbHelpers::assertTableContains($sql, [ '1; HELLO' ]);
+        DbHelpers::assertTableContains($sql, [ '1; CHILD' ]);
         DbHelpers::assertTableContains('select * from wordparents', [], 'no parents');
     }
 
     public function test_save_new_with_existing_parent()
     {
-        $this->formdata->term = 'PARENT';
-        $this->formdata->termlc = 'parent';
-        $pid = save_new_formdata($this->formdata);
-
-        $this->formdata->term = 'CHILD';
-        $this->formdata->termlc = 'child';
-        $this->formdata->parent_id = $pid;
-
-        $pid = save_new_formdata($this->formdata);
+        $pid = save_new_formdata($this->parent);
+        $this->child->parent_id = $pid;
+        save_new_formdata($this->child);
 
         $expected = [ '1; PARENT', '2; CHILD'];
         $sql = 'select WoID, WoText from words';
@@ -63,12 +61,45 @@ final class word_input_form_Test extends TestCase
         DbHelpers::assertTableContains('select * from wordparents', ['2; 1'], 'parent set');
     }
 
+    private function save_new_with_parent_and_child()
+    {
+        $pid = save_new_formdata($this->parent);
+        $this->child->parent_id = $pid;
+        return save_new_formdata($this->child);
+    }
+
+    /*
+    public function test_update_remove_parent()
+    {
+        $wid = $this->save_parent_and_child();
+        $this->formdata->wid = $wid;
+        $this->formdata->parent_id = 0;
+        $this->formdata->parent_text = '';
+        $this->formdata->translation = "new translation";
+        save_new_formdata($this->formdata);
+
+        $sql = "select WoTranslation from words where WoID = {$wid}";
+        DbHelpers::assertTableContains($sql, [ 'new translation' ]);
+        DbHelpers::assertTableContains('select * from wordparents', [], 'no parents');
+    }
+
+    public function test_update_change_parent()
+    {
+        $wid = $this->save_parent_and_child();
+        $this->formdata->wid = $wid;
+        $this->formdata->parent_id = 0;
+        $this->formdata->parent_text = '';
+        $this->formdata->translation = "new translation";
+        save_new_formdata($this->formdata);
+
+        $sql = "select WoTranslation from words where WoID = {$wid}";
+        DbHelpers::assertTableContains($sql, [ 'new translation' ]);
+        DbHelpers::assertTableContains('select * from wordparents', [], 'no parents');
+
+    }
+
     /** tests to do:
 - save new with new parent -- complicated
-
-- save existing, no parent (existing removed)
-- save existing, parent, relationship changed
-- existing, parent, new parent
 - existing, new parent, complicated
      */
 
