0c2c9d17c04f4f85b647c2d085d4a14e390e408d
Jeff Zohrab <jzohrab@gmail.com>
2022-11-24 14:24:22 -0600

    Set up basic loader data.

--- tests/word_input_form__Loader_Test.php
+++ tests/word_input_form__Loader_Test.php
@@ -9,6 +9,19 @@ final class word_input_form__Loader_Test extends word_input_form_TestBase {
 
     public function childSetUp() {
         // set up the text
+        $this->langid = (int) get_first_value("select LgID as value from languages");
+        $this->text = "Hola tengo un gato.  No tengo una lista.  Ella tiene una bebida.";
+        DbHelpers::add_text($this->text, $this->langid);
+
+        splitCheckText($this->text, $this->langid, 1);
+        $spot_check_sql = " select ti2woid, ti2seid, ti2order, ti2text from textitems2
+where ti2order in (1, 10, 25) order by ti2order";
+        $expected = [
+            "0; 1; 1; Hola",
+            "0; 2; 10; No",
+            "0; 3; 25; bebida"
+        ];
+        DbHelpers::assertTableContains($spot_check_sql, $expected);
     }
 
     public function test_dummy() {
@@ -16,4 +29,13 @@ final class word_input_form__Loader_Test extends word_input_form_TestBase {
         $this->assertTrue(true);
     }
 
+    /** tests to do
+     * normal cases:
+     * - wid in db (existing word) - gets all of its data
+     * - wid blank/not given, tid and ord given
+     * - wid not given, tid and ord given, but word already exists in db
+
+     * missing params cases:
+     * - missing wid = must have tid and ord
+     */
 }
