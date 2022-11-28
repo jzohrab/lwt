3fcbfa90a8d7b7d9a9cfcf6b8af8c1700a5a1c7d
Jeff Zohrab <jzohrab@gmail.com>
2022-11-28 13:42:40 -0600

    Fix tests.

--- tests/session_utility_Test.php
+++ tests/session_utility_Test.php
@@ -13,6 +13,7 @@ final class session_utility_Test extends TestCase
         // Set up db.
         DbHelpers::ensure_using_test_db();
         DbHelpers::clean_db();
+        DbHelpers::load_language_spanish();
 
         // Fake session and server info.
         $_SERVER['HTTP_HOST'] = 'localhost';
@@ -138,18 +139,21 @@ VALUES (42, 1), (42, 2), (42,99999)");
     /** Texts */
 
     public function test_archive_text_id() {
-        $newid = get_first_value("select ifnull(max(txid), 0) + 1 from texts");
+        $newid = get_first_value("select ifnull(max(txid), 0) + 1 as value from texts");
         $text = 'Hola, tengo un gato.';
         DbHelpers::add_text($text, $newid, 'mytesting');
         splitCheckText($text, 1, $newid);
 
         $sentencesql = "select * from sentences where SeTxID = $newid";
         $ti2sql = "select Ti2Order, Ti2Text from textitems2 where Ti2TxID = $newid";
+        $txsql = "select TxID, TxTitle from texts where TxID = $newid and TxArchived = 0";
         DbHelpers::assertRecordcountEquals($sentencesql, 1, "sentences pre");
-        DbHelpers::assertRecordcountEquals($ti2sql, 9, "ti2 pre");
+        DbHelpers::assertRecordcountEquals($ti2sql, 8, "ti2 pre");
+        DbHelpers::assertRecordcountEquals($txsql, 1, "tx pre");
 
         archive_text_id($newid);
         DbHelpers::assertRecordcountEquals($sentencesql, 0, "sentences post");
         DbHelpers::assertRecordcountEquals($ti2sql, 0, "ti2 post");
+        DbHelpers::assertRecordcountEquals($txsql, 0, "tx post");
     }
 }
