efd27b79779dd55eb5f41bf4541e45064123cac0
Jeff Zohrab <jzohrab@gmail.com>
2022-12-01 20:25:42 -0600

    Reorg code, extract method.

--- trans.php
+++ trans.php
@@ -19,29 +19,36 @@ require_once 'inc/session_utility.php';
 $i = $_REQUEST["i"];
 $t = $_REQUEST["t"];
 
-$sql = 'select SeText, LgGoogleTranslateURI from languages, sentences, textitems2 where Ti2SeID = SeID and Ti2LgID = LgID and Ti2TxID = ' . $t . ' and Ti2Order = ' . $i;
-$res = do_mysqli_query($sql);
-$record = mysqli_fetch_assoc($res);
-if ($record) {
-    $satz = $record['SeText'];
-    $trans = isset($record['LgGoogleTranslateURI']) ? $record['LgGoogleTranslateURI'] : "";
-    if(substr($trans, 0, 1) == '*') { $trans = substr($trans, 1); 
+
+function get_trans_url($trans) {
+    if(substr($trans, 0, 1) == '*') {
+        $trans = substr($trans, 1);
     }
-} else {
-    my_die("No results: $sql"); 
-}
-mysqli_free_result($res);
-if ($trans != '') {
-    /*
-      echo "{" . $i . "}<br />";
-      echo "{" . $t . "}<br />";
-      echo "{" . createTheDictLink($trans,$satz) . "}<br />";
-    */
     if (substr($trans, 0, 7) == 'ggl.php') {
         $trans = str_replace('?', '?sent=1&', $trans);
     }
-    header("Location: " . createTheDictLink($trans, $satz));
+    return $trans;
 }
+
+$sql = "select SeText, LgGoogleTranslateURI 
+from languages, sentences, textitems2 
+where Ti2SeID=SeID and Ti2LgID=LgID 
+and Ti2TxID = $t and Ti2Order = $i";
+$res = do_mysqli_query($sql);
+$record = mysqli_fetch_assoc($res);
+mysqli_free_result($res);
+
+if (! $record) {
+    my_die("No results: $sql");
+}
+
+$trans = isset($record['LgGoogleTranslateURI']) ? $record['LgGoogleTranslateURI'] : "";
+$trans = get_trans_url($trans);
+
+if ($trans != '') {
+    header("Location: " . createTheDictLink($trans, $record['SeText']));
+}
+
 exit();
 
 ?>
