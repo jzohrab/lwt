0d82c974d6b1f68b9e66f2bbb6f45013905c952d
Jeff Zohrab <jzohrab@gmail.com>
2022-12-07 16:27:38 -0600

    Fix text form field names.

--- src/Form/TextType.php
+++ src/Form/TextType.php
@@ -12,15 +12,15 @@ class TextType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('TxLgID')
-            ->add('TxTitle')
-            ->add('TxText')
-            ->add('TxAnnotatedText')
-            ->add('TxAudioURI')
-            ->add('TxSourceURI')
-            ->add('TxPosition')
-            ->add('TxAudioPosition')
-            ->add('TxArchived')
+            ->add('LgID')
+            ->add('Title')
+            ->add('Text')
+            ->add('AnnotatedText')
+            ->add('AudioURI')
+            ->add('SourceURI')
+            ->add('Position')
+            ->add('AudioPosition')
+            ->add('Archived')
         ;
     }
 
