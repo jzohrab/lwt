11dc820e1ddeb35ae2d9071c13bf7c36bdfebaf8
Jeff Zohrab <jzohrab@gmail.com>
2022-12-10 21:34:37 -0600

    Tough case, works.

--- tests/src/Entity/Sentence_getRenderableTextItems_Test.php
+++ tests/src/Entity/Sentence_getRenderableTextItems_Test.php
@@ -83,23 +83,44 @@ class Sentence_getRenderableTextItems_Test extends TestCase
     }
 
 
-    public function DISABLED_test_multiword_items_cover_other_items()
+    /* From the class documentation:
+     *
+     * Graphically, suppose we had the following text items, where A-I are
+     * WordCount 0 or WordCount 1, and J-M are multiwords:
+     *
+     *  A   B   C   D   E   F   G   H   I
+     *    |---J---|   |---------K---------|
+     *                    |---L---|
+     *        |-----M---|
+     *
+     * J contains B and C, so B and C should not be rendered.
+     * 
+     * K contains E-I and also L, so none of those should be rendered.
+     *
+     * M is _not_ contained by anything else, so it should be rendered.
+     */
+    public function test_crazy_case()
     {
         $data = [
-            [ 1, 'some', 1 ],
-            [ 5, 'here', 1 ],
-            [ 4, ' ', 0 ],
-            [ 3, 'data', 1 ],
-            [ 2, ' ', 0 ],
-            [ 3, 'data here', 2 ],  // <<<
-            [ 6, '.', 0 ]
+            [ 1, 'A', 1 ],
+            [ 2, 'B', 1 ],
+            [ 3, 'C', 1 ],
+            [ 4, 'D', 1 ],
+            [ 5, 'E', 1 ],
+            [ 6, 'F', 1 ],
+            [ 7, 'G', 1 ],
+            [ 8, 'H', 1 ],
+            [ 9, 'I', 1 ],
+            [ 2, 'J', 2 ],
+            [ 5, 'K', 5 ],
+            [ 6, 'L', 2 ],
+            [ 3, 'M', 3 ]
         ];
         $sentence = $this->make_sentence($data);
 
         $sentence->render($this->fakeRender);
-        $expected = '[some-1][ -0][data here-2][.-0]';
+        $expected = '[A-1][J-2][M-3][K-5]';
         $this->assertEquals($this->rendered, $expected);
     }
-
 
 }
