40b9d2d81ba307fcc73888dd76515d04acce044b
Jeff Zohrab <jzohrab@gmail.com>
2022-12-11 16:07:24 -0600

    Fix searching.

--- devscripts/findstring.sh
+++ devscripts/findstring.sh
@@ -10,9 +10,9 @@ SEARCHFOR="$@"
 function runsearch() {
     pushd $1 > /dev/null
     echo
-    echo "$SEARCHFOR IN $1"
+    echo "$SEARCHFOR IN $1 at depth $2"
     echo
-    find . -name '*.*' -maxdepth $2 -print0 | xargs -0 grep "$SEARCHFOR" 2>/dev/null | grep -v vendor | grep -v docs | grep -v ^\./js/ | grep -v "Binary file" | grep -v ^\./themes/ | grep -v src/themes | grep -v src/css | grep -v ^\./css
+    find . -name '*.*' -maxdepth $2 -print0 | xargs -0 grep "$SEARCHFOR" 2>/dev/null #  | grep -v vendor | grep -v docs | grep -v ^\./js/ | grep -v "Binary file" | grep -v ^\./themes/ | grep -v src/themes | grep -v src/css | grep -v ^\./css
     popd > /dev/null
     echo
 }
