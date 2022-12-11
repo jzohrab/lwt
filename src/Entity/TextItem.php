2aa8ec16942132ab9c40b7eb03920e1c8d2e2bac
Jeff Zohrab <jzohrab@gmail.com>
2022-12-11 13:19:06 -0600

    Add classname.

--- src/Entity/TextItem.php
+++ src/Entity/TextItem.php
@@ -9,4 +9,63 @@ class TextItem
     public string $Text;
     public int $WordCount;
 
+    public string $TextLC;
+    public int $SeID;
+    public int $IsWord;
+    public int $TextLength;
+
+    public ?int $WoID;
+    public ?string $WoText;
+    public ?int $WoStatus;
+    public ?string $WoTranslation;
+    public ?string $WoRomanization;
+    public ?string $Tags;
+
+    public ?int $ParentWoID;
+    public ?string $ParentWoTextLC;
+    public ?string $ParentWoTranslation;
+    public ?string $ParentTags;
+
+
+    private function strToHex($string): string
+    {
+        $hex='';
+        for ($i=0; $i < strlen($string); $i++) {
+            $h = dechex(ord($string[$i]));
+            $h = str_pad($h, 2, '0', STR_PAD_LEFT);
+            $hex .= $h; 
+        }
+        return strtoupper($hex);
+    }
+
+    /**
+     * Returns "TERM" + non-hexified string.
+     * Escapes everything to "HEXxx" but not 0-9, a-z, A-Z, and unicode >= (hex 00A5, dec 165)
+     */
+    public function getTermClassname(): string
+    {
+        $string = $this->TextLC;
+        $length = mb_strlen($string, 'UTF-8');
+        $r = '';
+        for ($i=0; $i < $length; $i++)
+        {
+            $c = mb_substr($string, $i, 1, 'UTF-8');
+            $add = $c;
+
+            $o = ord($c);
+            if (
+                ($o < 48) ||
+                ($o > 57 && $o < 65) ||
+                ($o > 90 && $o < 97) ||
+                ($o > 122 && $o < 165)
+            ) {
+                $add = 'HEX' . $this->strToHex($c);
+            }
+
+            $r .= $add; 
+        }
+
+        return "TERM{$r}";
+    }
+
 }
