163f5a0d9982d9c9bf968d14dca9c025a3c7cda0
Jeff Zohrab <jzohrab@gmail.com>
2022-12-12 17:54:09 -0600

    Term repo tests.

--- tests/src/Repository/TermRepository_Test.php
+++ tests/src/Repository/TermRepository_Test.php
@@ -11,9 +11,31 @@ final class TermRepository_Test extends DatabaseTestBase
 
     public function childSetUp() {
         $this->load_languages();
+
+        $tag = new TermTag();
+        $tag->setText("tag");
+        $tag->setComment("tag comment");
+        $this->termtag_repo->save($tag, true);
+        $this->tag = $tag;
+
+        $p = new Term();
+        $p->setLanguage($this->spanish);
+        $p->setText("PARENT");
+        $p->setStatus(1);
+        $p->setWordCount(1);
+        $this->term_repo->save($p, true);
+        $this->p = $p;
+
+        $p2 = new Term();
+        $p2->setLanguage($this->spanish);
+        $p2->setText("OTHER");
+        $p2->setStatus(1);
+        $p2->setWordCount(1);
+        $this->term_repo->save($p2, true);
+        $this->p2 = $p2;
     }
 
-    public function disab_test_create_and_save()
+    public function test_create_and_save()
     {
         $t = new Term();
         $t->setLanguage($this->spanish);
@@ -26,37 +48,25 @@ final class TermRepository_Test extends DatabaseTestBase
 
         $this->assertEquals($t->getTextLC(), 'hola', "sanity check of case");
 
-        $sql = "select WoID, WoText, WoTextLC from words";
-        $expected = [ "1; HOLA; hola" ];
+        $sql = "select WoText, WoTextLC from words where WoID={$t->getID()}";
+        $expected = [ "HOLA; hola" ];
         DbHelpers::assertTableContains($sql, $expected, "sanity check on save");
     }
 
 
     public function test_word_with_parent_and_tags()
     {
-        $tag = new TermTag();
-        $tag->setText("tag");
-        $tag->setComment("tag comment");
-        $this->termtag_repo->save($tag, true);
-
-        $p = new Term();
-        $p->setLanguage($this->spanish);
-        $p->setText("PARENT");
-        $p->setStatus(1);
-        $p->setWordCount(1);
-        $this->term_repo->save($p, true);
-
         $t = new Term();
         $t->setLanguage($this->spanish);
         $t->setText("HOLA");
         $t->setStatus(1);
         $t->setWordCount(1);
-        $t->setParent($p);
-        $t->addTermTag($tag);
+        $t->setParent($this->p);
+        $t->addTermTag($this->tag);
         $this->term_repo->save($t, true);
 
         $sql = "select WoID, WoText, WoTextLC from words";
-        $expected = [ "1; PARENT; parent", "2; HOLA; hola" ];
+        $expected = [ "1; PARENT; parent", "2; OTHER; other", "3; HOLA; hola" ];
         DbHelpers::assertTableContains($sql, $expected, "sanity check on save");
 
         // Hacky sql check.
