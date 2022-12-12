415dd316008d4e513562b996e8eec1422318c772
Jeff Zohrab <jzohrab@gmail.com>
2022-12-12 11:36:24 -0600

    Use DatabaseTestBase.

--- tests/src/Repository/TextRepository_getSentences_Test.php
+++ tests/src/Repository/TextRepository_getSentences_Test.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
 require_once __DIR__ . '/../../db_helpers.php';
-require_once __DIR__ . '/RepositoryTestBase.php';
+require_once __DIR__ . '/../../DatabaseTestBase.php';
 
 use App\Entity\Text;
     
-final class TextRepository_getSentences_Test extends RepositoryTestBase
+final class TextRepository_getSentences_Test extends DatabaseTestBase
 {
 
     public function childSetUp(): void
