90fb2a8ce28b056d57ff7f06676fa80f01cbbfab
Jeff Zohrab <jzohrab@gmail.com>
2022-12-11 19:58:41 -0600

    Handle drag/drop multiword term url creation.

--- public/js/lute.js
+++ public/js/lute.js
@@ -7,16 +7,9 @@
  */
 function prepareTextInteractions(textid) {
   $('.word').on('click', word_clicked);
-  $('.mword').on('click', word_clicked);
-
+  $('.word').mousedown(select_started);
+  $('.word').mouseup(select_ended);
   /*
-    $('#thetext').on('selectstart','span',false).on(
-        'mousedown','.wsty',
-        {annotation: ANNOTATIONS_MODE}, 
-        mword_drag_n_drop_select);
-    $('#thetext').on('click', '.mword', mword_click_event_do_text_text);
-    $('.word').on('dblclick', word_dblclick_event_do_text_text);
-    $('#thetext').on('dblclick', '.mword', word_dblclick_event_do_text_text);
     $(document).on('keydown', keydown_event_do_text_text);
     $('#thetext').hoverIntent(
         {
@@ -43,10 +36,47 @@ function word_clicked(e) {
   $(this).addClass('kwordmarked');
 
   let av = (s) => $(this).attr(s);
-
   const url = 'edit_word.php?tid=' + av('tid') + '&ord=' + av('data_order') + '&wid=' + av('data_wid');
   alert(url);
   // TODO: showRightFrames
   
   return false;
+}
+
+var selection_start_el = null;
+
+function select_started(e) {
+  selection_start_el = $(this);
+  console.log('started with ' + $(this).html());
+}
+
+function select_ended(e) {
+  console.log('ended with ' + $(this).html());
+  if (selection_start_el.attr('id') == $(this).attr('id')) {
+    return;
+  }
+
+  if (selection_start_el.attr('seid') != $(this).attr('seid')) {
+    alert("Selections cannot span sentences.");
+    return;
+  }
+
+  const startord = parseInt(selection_start_el.attr('data_order'));
+  const endord = parseInt($(this).attr('data_order'));
+  const selected = $("span.word").filter(function() {
+    const ord = $(this).attr("data_order");
+    return ord >= startord && ord <= endord;
+  });
+  const text = selected.toArray().map((el) => $(el).text()).join(' ');
+  console.log(text);
+
+  if (text.length > 250) {
+    alert(`Selections can be max length 250 chars ("${text}" is ${text.length} chars)`);
+    return;
+  }
+
+  let av = (s) => selection_start_el.attr(s);
+  const url = `edit_word.php?tid=${av('tid')}&ord=${av('data_order')}&wid=&text=${encodeURIComponent(text)}`;
+  alert(url);
+  // TODO: showRightFrames
 }
