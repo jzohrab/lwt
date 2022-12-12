6a247de0d5585ec5ceec14d5de79edec7d95aaa3
Jeff Zohrab <jzohrab@gmail.com>
2022-12-12 10:17:36 -0600

    Simplify test data load.

--- tests/src/Repository/TextRepository_getSentences_Test.php
+++ tests/src/Repository/TextRepository_getSentences_Test.php
@@ -37,43 +37,6 @@ final class TextRepository_getSentences_Test extends RepositoryTestBase
         $t->setLanguage($lang);
         $this->text_repo->save($t, true);
         $this->text = $t;
-
-        // Initial double-check.
-        $allti2 = "select Ti2SeID, Ti2Order, Ti2Text from textitems2 order by Ti2Order, Ti2WordCount DESC";
-        DbHelpers::assertRecordcountEquals($allti2, 28, 'setup, terms included');
-
-        $expected = [
-"1; 1; Hola",
-"1; 2;  ",
-"1; 3; tengo",
-"1; 4;  ",
-"1; 5; un gato",
-"1; 5; un",
-"1; 6;  ",
-"1; 7; gato",
-"1; 8; .",
-"2; 9;  ",
-"2; 10; No",
-"2; 11;  ",
-"2; 12; tengo",
-"2; 13;  ",
-"2; 14; una",
-"2; 15;  ",
-"2; 16; lista",
-"2; 17; .",
-"3; 18; ¶",
-"4; 19; Ella",
-"4; 20;  ",
-"4; 21; tiene una",
-"4; 21; tiene",
-"4; 22;  ",
-"4; 23; una",
-"4; 24;  ",
-"4; 25; bebida",
-"4; 26; .",
-
-        ];
-        DbHelpers::assertTableContains($allti2, $expected, 'terms');
     }
 
 
