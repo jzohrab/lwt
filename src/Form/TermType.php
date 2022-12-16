aa7ee1a427e430b1a5473c87e6293bce3bfd224e
Jeff Zohrab <jzohrab@gmail.com>
2022-12-16 15:34:21 -0600

    Use textarea for translation.

--- src/Form/TermType.php
+++ src/Form/TermType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -55,7 +56,7 @@ class TermType extends AbstractType
                   ]
             )
             ->add('Translation',
-                  TextType::class,
+                  TextareaType::class,
                   [ 'label' => 'Translation',
                     'attr' => [ 'class' => 'form-text' ],
                     'required' => false
