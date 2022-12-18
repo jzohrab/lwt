a47cb275b41ab44735255304a4ae2fd616131cdf
Jeff Zohrab <jzohrab@gmail.com>
2022-12-18 07:58:47 -0600

    Remove profiler hack which was tripping up kernel autowiring.

--- src/Controller/TermController.php
+++ src/Controller/TermController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Profiler\Profiler;
 
 #[Route('/term')]
 class TermController extends AbstractController
