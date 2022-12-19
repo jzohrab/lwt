91a575f46da8eeaa9cdf3836e4a619a8f3f718e3
Jeff Zohrab <jzohrab@gmail.com>
2022-12-18 20:47:17 -0600

    New TextStatsCache class.

---
+++ src/Domain/TextStatsCache.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Domain;
+
+use App\Entity\Text;
+use App\Entity\Term;
+use App\Entity\Language;
+use App\Repository\TermRepository;
+
+require_once __DIR__ . '/../../connect.inc.php';
+
+
+class TextStatsCache {
+
+    /**
+     * refresh records in textstatscache.
+     *
+     * When listing texts, it's far too slow to query and rebuild
+     * stats all the time.
+     */
+    public static function refresh() {
+
+        
+        // TODO:storedproc Replace temp table with stored proc.
+        //
+        // Using a temp table to determine which texts to update.
+        // I tried using left joins back to textstatscache, but it
+        // was slow, despite indexing.  There is probably a better
+        // way to do this, but this works for now.
+        //
+        // Ideally, this would be a temp table ... but then the stats update
+        // query complains about "reopening the table", as it's used several
+        // times in the query.
+        //
+        // This could be moved to a stored procedure, but this is good
+        // enough for now.
+
+        $sqls = [
+            // Temp table of textids.
+            "drop table if exists TEMPupdateStatsTxIDs",
+            "create table TEMPupdateStatsTxIDs (TxID int primary key)",
+
+            // Load IDs to update.
+            "insert into TEMPupdateStatsTxIDs
+select t.TxID from texts t
+left join textstatscache c on c.TxID = t.TxID
+where c.TxID is null
+and t.TxArchived = 0;",
+
+            // Load stats.
+            "insert into textstatscache (
+  TxID,
+  wordcount,
+  distinctterms,
+  multiwordexpressions,
+  sUnk,
+  s1,
+  s2,
+  s3,
+  s4,
+  s5,
+  sIgn,
+  sWkn
+)
+SELECT
+t.TxID As TxID,
+
+wordcount.n as wordcount,
+coalesce(distinctterms.n, 0) as distinctterms,
+coalesce(mwordexpressions.n, 0) as multiwordexpressions,
+sUnk, s1, s2, s3, s4, s5, sIgn, sWkn
+
+FROM texts t
+inner join TEMPupdateStatsTxIDs u on u.TxID = t.TxID
+
+LEFT OUTER JOIN (
+  SELECT Ti2TxID as TxID, COUNT(*) AS n
+  FROM textitems2
+  inner join TEMPupdateStatsTxIDs u on u.TxID = textitems2.Ti2TxID
+  WHERE Ti2WordCount = 1
+  GROUP BY Ti2TxID
+) AS wordcount on wordcount.TxID = t.TxID
+
+LEFT OUTER JOIN (
+  SELECT Ti2TxID as TxID, COUNT(distinct Ti2WoID) AS n
+  FROM textitems2
+  inner join TEMPupdateStatsTxIDs u on u.TxID = textitems2.Ti2TxID
+  WHERE Ti2WoID <> 0
+  GROUP BY Ti2TxID
+) AS distinctterms on distinctterms.TxID = t.TxID
+
+LEFT OUTER JOIN (
+  SELECT Ti2TxID AS TxID, COUNT(DISTINCT Ti2WoID) as n
+  FROM textitems2
+  inner join TEMPupdateStatsTxIDs u on u.TxID = textitems2.Ti2TxID
+  WHERE Ti2WordCount > 1
+  GROUP BY Ti2TxID
+) AS mwordexpressions on mwordexpressions.TxID = t.TxID
+
+LEFT OUTER JOIN (
+
+      SELECT TxID,
+      SUM(CASE WHEN status=0 THEN c ELSE 0 END) AS sUnk,
+      SUM(CASE WHEN status=1 THEN c ELSE 0 END) AS s1,
+      SUM(CASE WHEN status=2 THEN c ELSE 0 END) AS s2,
+      SUM(CASE WHEN status=3 THEN c ELSE 0 END) AS s3,
+      SUM(CASE WHEN status=4 THEN c ELSE 0 END) AS s4,
+      SUM(CASE WHEN status=5 THEN c ELSE 0 END) AS s5,
+      SUM(CASE WHEN status=98 THEN c ELSE 0 END) AS sIgn,
+      SUM(CASE WHEN status=99 THEN c ELSE 0 END) AS sWkn
+
+      FROM (
+      SELECT Ti2TxID AS TxID, WoStatus AS status, COUNT(*) as c
+      FROM textitems2
+      inner join TEMPupdateStatsTxIDs u on u.TxID = textitems2.Ti2TxID
+      INNER JOIN words ON WoID = Ti2WoID
+      WHERE Ti2WoID <> 0
+      GROUP BY Ti2TxID, WoStatus
+
+      UNION
+      SELECT Ti2TxID as TxID, 0 as status, COUNT(*) as c
+      FROM textitems2
+      inner join TEMPupdateStatsTxIDs u on u.TxID = textitems2.Ti2TxID
+      WHERE Ti2WoID = 0 AND Ti2WordCount = 1
+      GROUP BY Ti2TxID
+  
+      ) rawdata
+      GROUP BY TxID
+) AS statuses on statuses.TxID = t.TxID",
+
+            // cleanup.
+            "drop table if exists TEMPupdateStatsTxIDs"
+        ];
+
+        foreach ($sqls as $sql)
+            TextStatsCache::exec_sql($sql);
+    }
+
+    public static function markAsStale(array $text_ids) {
+        $ids = implode(', ', $text_ids);
+        $sql = "DELETE from textstatscache where TxID in ({$ids})";
+        TextStatsCache::exec_sql($sql);
+    }
+
+    // Private.
+
+    private static function getConnection()
+    {
+        global $userid, $passwd, $server, $dbname; // From connect.inc.php
+        $conn = @mysqli_connect($server, $userid, $passwd, $dbname);
+        return $conn;
+    }
+
+    private static function exec_sql($sql, $params = null) {
+        // echo $sql . "\n";
+        $conn = TextStatsCache::getConnection();
+        $stmt = $conn->prepare($sql);
+        if (!$stmt) {
+            throw new \Exception($conn->error);
+        }
+        if ($params) {
+            $stmt->bind_param(...$params);
+        }
+        if (!$stmt->execute()) {
+            throw new \Exception($stmt->error);
+        }
+        return $stmt->get_result();
+    }
+
+}
---
+++ tests/src/Domain/TextStatsCache_Test.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../../db_helpers.php';
+require_once __DIR__ . '/../../DatabaseTestBase.php';
+
+use App\Entity\Text;
+use App\Domain\TextStatsCache;
+
+final class TextStatsCache_Test extends DatabaseTestBase
+{
+
+    public function childSetUp(): void
+    {
+        // Set up db.
+        DbHelpers::load_language_spanish();
+        $this->langid = (int) get_first_value("select LgID as value from languages");
+
+        $lid = $this->langid;
+        DbHelpers::add_word($lid, "Un gato", "un gato", 1, 2);
+        DbHelpers::add_word($lid, "lista", "lista", 1, 1);
+        DbHelpers::add_word($lid, "tiene una", "tiene una", 1, 2);
+
+        // A parent term.
+        DbHelpers::add_word($lid, "listo", "listo", 1, 1);
+        DbHelpers::add_word_parent($lid, "lista", "listo");
+
+        // Some tags for fun.
+        DbHelpers::add_word_tag($lid, "Un gato", "furry");
+        DbHelpers::add_word_tag($lid, "lista", "adj");
+        DbHelpers::add_word_tag($lid, "lista", "another");
+        DbHelpers::add_word_tag($lid, "listo", "padj1");
+        DbHelpers::add_word_tag($lid, "listo", "padj2");
+    }
+
+
+    public function test_saving_text_loads_cache()
+    {
+        DbHelpers::exec_sql("delete from textstatscache");
+        DbHelpers::assertRecordcountEquals("textstatscache", 0, "nothing loaded");
+
+        $t = new Text();
+        $t->setTitle("Hola.");
+        $t->setText("Hola tengo un gato.  No tengo una lista.\nElla tiene una bebida.");
+        $lang = $this->language_repo->find($this->langid);
+        $t->setLanguage($lang);
+        $this->text_repo->save($t, true);
+        $this->text = $t;
+
+        DbHelpers::assertRecordcountEquals("textstatscache", 1, "one record after save");
+    }
+
+    public function test_mark_as_stale_and_refresh_updates_stats()
+    {
+        $t = new Text();
+        $t->setTitle("Hola.");
+        $t->setText("Hola tengo un gato.  No tengo una lista.\nElla tiene una bebida.");
+        $lang = $this->language_repo->find($this->langid);
+        $t->setLanguage($lang);
+        $this->text_repo->save($t, true);
+        $this->text = $t;
+
+        $sql = "select sUnk from textstatscache where TxID = {$t->getID()}";
+        DbHelpers::assertTableContains($sql, [ "11" ], "one record after save");
+
+        DbHelpers::exec_sql("update textstatscache set sUnk = 999");
+        DbHelpers::assertTableContains($sql, [ "999" ], "updated");
+
+        TextStatsCache::refresh();
+        DbHelpers::assertTableContains($sql, [ "999" ], "refreshed, but still old value");
+
+        TextStatsCache::markAsStale([ $t->getID() ]);
+        TextStatsCache::refresh();
+        DbHelpers::assertTableContains($sql, [ "11" ], "Updated");
+    }
+
+
+}
