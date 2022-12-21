444103240770484e8834408c4931d31306028d5b
Jeff Zohrab <jzohrab@gmail.com>
2022-12-21 14:01:00 -0600

    New failing test for status updates.

--- tests/src/Repository/TermRepository_WoStatusChanged_Test.php
+++ tests/src/Repository/TermRepository_WoStatusChanged_Test.php
@@ -13,26 +13,53 @@ final class TermRepository_WoStatusChanged_Test extends DatabaseTestBase
 
     public function childSetUp() {
         $this->load_languages();
-    }
-
-    public function test_creating_new_term_sets_field() {
         $t = new Term();
         $t->setLanguage($this->spanish);
         $t->setText("PARENT");
         $t->setStatus(1);
         $t->setWordCount(1);
         $this->term_repo->save($t, true);
+        $this->term = $t;
+    }
 
-        $sql = "select WoCreated, WoStatusChanged from words where WoID = {$t->getID()}";
+    private function set_WoStatusChanged($newval) {
+        $sql = "update words set WoStatusChanged = '" . $newval . "'";
+        DbHelpers::exec_sql($sql);
+    }
 
-        // get field, should be set
+    private function assertUpdated($msg = '') {
+        // Cleanest way to check is to timestampdiff ... can't check
+        // vs current time because comparison would change with clock
+        // ticks.  Yes, this is totally geeky.
+        $sql = "SELECT
+          WoStatusChanged,
+          TIMESTAMPDIFF(SECOND, WoStatusChanged, NOW()) as diffsecs
+          FROM words where WoID = {$this->term->getID()}";
+        $rec = DbHelpers::exec_sql_get_result($sql);
+        $a = mysqli_fetch_assoc($rec);
+        $diff = intval($a['diffsecs']);
+        $msg = $msg . " Was updated (set to " . $a['WoStatusChanged'] . ")";
+        $this->assertTrue($diff < 10, $msg);
+    }
+    
+    public function test_creating_new_term_sets_field() {
+        $sql = "select WoCreated, WoStatusChanged from words
+                where WoID = {$this->term->getID()}";
         $rec = DbHelpers::exec_sql_get_result($sql);
         $a = mysqli_fetch_assoc($rec);
         $this->assertEquals($a['WoCreated'], $a['WoStatusChanged']);
     }
 
-    // new term status = today
+    public function test_updating_status_updates_field() {
+        $this->set_WoStatusChanged("1970-01-01");
+        $this->term->setStatus(2);
+        $this->term_repo->save($this->term, true);
+        $this->assertUpdated();
+    }
+    
     // term update status = today
+    //         $rec = DbHelpers::exec_sql("update words set WoStatusChanged = '1970-01-01'");
+
     // term change status to same = not changed
     // sql update to same = not changed
     // sql update = changed
