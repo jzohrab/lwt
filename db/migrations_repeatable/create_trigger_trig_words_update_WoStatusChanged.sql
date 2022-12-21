823e3d56c85ce26a1fcf9ee23d9acb9d7f7d0682
Jeff Zohrab <jzohrab@gmail.com>
2022-12-21 14:15:33 -0600

    Create trigger, tests passing.

--- db/migrations_repeatable/create_trigger_trig_words_update_WoStatusChanged.sql
+++ db/migrations_repeatable/create_trigger_trig_words_update_WoStatusChanged.sql
@@ -5,14 +5,10 @@
 -- a dump file is imported.
 DROP TRIGGER IF EXISTS trig_words_update_WoStatusChanged;
 
-DELIMITER //
-
 CREATE TRIGGER trig_words_update_WoStatusChanged BEFORE UPDATE ON words
 FOR EACH ROW
 BEGIN
     IF new.WoStatus <> old.WoStatus THEN
         SET new.WoStatusChanged = CURRENT_TIMESTAMP;
     END IF;
-END;//
-
-DELIMITER ;
+END;
